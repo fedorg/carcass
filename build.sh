85be03f66d7b1a6ae4273f6b8c43e86b6757e8ed
Fedor Golishevskiy <fed0000r@gmail.com>
2019-08-05 12:40:25 +0300

    fix circleci compilation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-GOOS=windows go build -i -v -ldflags="-X main.CARCASS_VERSION=$(git describe --always --tags --long --dirty)"
+CGO_ENABLED=0 GO111MODULE=on GOARCH=amd64 GOOS=windows go build -i -v -ldflags="-X main.CARCASS_VERSION=$(git describe --always --tags --long --dirty)"
